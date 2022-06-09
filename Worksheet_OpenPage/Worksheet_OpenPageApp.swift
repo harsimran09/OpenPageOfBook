6c731ae7dc4e2aebd183d514f2965bb3b5fa9126
Harsimran Kaur <harsimran.kaur1@whitehatjr.com>
2022-06-09 18:28:11 +0530

    Initial Commit

---
+++ Worksheet_OpenPage/Worksheet_OpenPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Worksheet_OpenPageApp.swift
+//  Worksheet_OpenPage
+//
+//  Created by ITRS-1519 on 09/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Worksheet_OpenPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
